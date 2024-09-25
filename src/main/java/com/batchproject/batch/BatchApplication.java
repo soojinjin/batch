766820513e7c9baa9188fe68d445e14d4b6965fa
[soojinjin] <[imsoojin321@naver.com]>
2024-09-25 21:32:58 +0900

    batch

---
+++ src/main/java/com/batchproject/batch/BatchApplication.java
@@ -0,0 +1,15 @@
+package com.batchproject.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BatchApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/main/java/com/batchproject/batch/JobConfiguration.java
@@ -0,0 +1,55 @@
+package com.batchproject.batch;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.rmi.UnexpectedException;
+
+@Slf4j
+@Configuration
+public class JobConfiguration {
+    @Bean
+    public Job job(JobRepository jobRepository, Step step){
+        return new JobBuilder("job-chunk",jobRepository)
+                .start(step)
+                .build();
+    }
+    @Bean
+    public Step step(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager){
+     final ItemReader<Integer> itemReader = new ItemReader<>(){
+
+        private int count=0;
+        @Override
+        public Integer read() throws Exception, UnexpectedException, ParseException, NonTransientResourceException{
+            count++;
+            log.info("Read {}", count);
+
+            if(count ==15)
+                return null;
+            return count;
+
+        }
+    };
+
+     return new StepBuilder("step",jobRepository)
+             .chunk(10,platformTransactionManager)
+             .reader(itemReader)
+            // .processor()
+             .writer(read -> {})
+             .build();
+    }
+}
